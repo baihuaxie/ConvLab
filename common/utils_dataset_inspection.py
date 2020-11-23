d732586385416dbeeca0f5b487a6286cdbc5802c
Baihua Xie <xie_baihua@163.com>
2020-11-24 07:55:33 +0800

    add save sampled images as figures

--- common/utils_dataset_inspection.py
+++ common/utils_dataset_inspection.py
@@ -18,7 +18,13 @@ def get_classes(dataset, datadir):
     """
     return class names from dataset meta file as a list of strings
     """
-    meta_path = op.join(datadir, meta_mapping[dataset])
+    try:
+        meta_path = op.join(datadir, meta_mapping[dataset])
+    except KeyError:
+        raise "dataset {} meta path not registered".format(dataset)
+
+    assert op.isfile(meta_path), "No meta file found at {} for dataset {}".format( \
+        meta_path, dataset)
     with open(meta_path, 'rb') as fo:
         dct = pickle.load(fo, encoding='ASCII')
     for key, value in dct.items():
@@ -44,7 +50,7 @@ def show_images(img):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 
-def show_labelled_images(img, labels, classes, nrows=8, ncols=8):
+def show_labelled_images(img, labels, classes, nrows=8, ncols=8, savepath=None):
     """
     print images in a grid with actual labels as image titles
 
@@ -54,6 +60,7 @@ def show_labelled_images(img, labels, classes, nrows=8, ncols=8):
         classes: (list of str) a list of strings for class names
         nrows: (int) # of rows in the grid; default=8
         ncols: (int) # of cols in the grid; default=8
+        savepath: (str) path to save figures
 
     Note:
     - by default, # of grids to be saved = B / (nrows * ncols)
@@ -66,7 +73,7 @@ def show_labelled_images(img, labels, classes, nrows=8, ncols=8):
         raise TypeError("Image type {} not recognized".format(type(img)))
 
     grid_sz = ncols * nrows
-    fig = plt.figure(figsize=(nrows*1.5, ncols*1.5))
+    fig = plt.figure(figsize=(ncols*1.5, nrows*1.5))
     for idx in range(0, npimg.shape[0]):
         ax = fig.add_subplot(nrows, ncols, idx % grid_sz + 1, xticks=[], yticks=[])
         # add image
@@ -75,17 +82,13 @@ def show_labelled_images(img, labels, classes, nrows=8, ncols=8):
         try:
             ax.set_title(classes[int(labels[idx].item())])
         except IndexError:
-            raise("label index {} out of range for {} number of \
-                classes".format(labels[idx], len(classes)))
-        # create a new fig object once grid_sz is reached
-        if (idx + 1) % grid_sz == 0:
-            fig.subplots_adjust(hspace=1)
+            raise "label index {} out of range for {} number of \
+                classes".format(labels[idx], len(classes))
+        # save figure when current grid is full or when end of loop reached
+        # create a new fig object once current grid is full
+        if (idx + 1) % grid_sz == 0 or idx == npimg.shape[0]-1:
+            fig.subplots_adjust(hspace=0.5)
+            plt.savefig(savepath + "_{}.png".format(idx // grid_sz))
             plt.show()
-            fig = plt.figure(figsize=(nrows*1.5, ncols*1.5))
-    plt.show()
-    
-
-
-
-
-
+            if (idx + 1) % grid_sz == 0:
+                fig = plt.figure(figsize=(ncols*1.5, nrows*1.5))
